172657fe921f40a500ae334b240078a89feab29a
徐明 <xuming@xumingdeMacBook-Pro.local>
2025-07-09 11:04:52 +0800

    Setup build for decorator lib

--- decorators/server.ts
+++ decorators/server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
+import cors from 'cors';
 
 export function Server (target: any) {
   const app = express();
-  const cors = require('cors');
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
