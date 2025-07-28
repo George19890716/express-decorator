d5959f1f4e771a327cf7fb78bfdfc67b357a48bb
Ming <xuming@xumingdeMacBook-Pro.local>
2025-07-28 15:12:43 +0800

    Integration chalk to express-spring

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import cors from 'cors';
 import express from 'express';
 import path from 'path';
@@ -12,7 +13,7 @@ export function ExpressApplication(target: any) {
     const valuesPath = path.join(process.cwd(), valuesFolder);
     initialValues(valuesPath);
 
-    console.log(`Data in "${valuesFolder}" folder has been initialized!`);
+    console.log(chalk.green(`Data in "${valuesFolder}" folder has been initialized!`));
 
     const app = express();
 
@@ -24,10 +25,10 @@ export function ExpressApplication(target: any) {
     app.set('port', port);
 
     app.listen(app.get('port'), () => {
-      console.log('Mock Backend with Express Spring startup in ', app.get('port'));
+      console.log(chalk.green('Mock Backend with Express Spring startup in ', app.get('port')));
     });
   } catch(e) {
-    console.error('Failed to startup Mock Backend with Express Spring!');
-    console.error(e?.message ?? e);
+    console.error(chalk.red('Failed to startup Mock Backend with Express Spring!'));
+    console.error(chalk.red(e?.message ?? e));
   }
 }
